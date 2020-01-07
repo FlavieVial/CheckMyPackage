da5f9aa7d1d52df29cd267dc89ebd5637c70648d
Emma Rand <tmpq7208@its.york.ac.uk>
2020-01-07 12:22:07 +0000

    document help for sum_squares function

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,4 +1,13 @@
 #sum of squares function
+#' Sum the square of difference between mean and your sample
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares(rnorm(100,20,2))
 sum_squares <- function(v) {
   sum((v - mean(v))^2)
 }
